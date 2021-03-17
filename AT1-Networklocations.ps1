1641b7f8ac1b38029237bde6ef9e1bc4e50ee6d3
sandereek <saner.eek@hcs-company.com>
2021-03-17 02:37:22 +0100

    Add network locations to Windows Server

--- AT1-Networklocations.ps1
+++ AT1-Networklocations.ps1
@@ -1,16 +1,3 @@
-
-<#
-    Author: 
-        
-        Sander Eek, 2020
-    
-    Description:
-
-    Includes the PowerShell Functions used to add a UNC path as a Network Location to Windows Server (also works on Windows Clients) and
-    then calls the Add-HEINEKENNetworklocation function with the UNC path (folders are supported) that need to be added as a network location
-    
-#>
-
 . .\homaas_v2\Add-NetworkLocation_for_LinkFixer.ps1
 
 Add-HEINEKENNetworkLocation -networkLocation "\\at1slnz100.at1.heiway.net\dat\linien\oi\03_Sekretariat"
