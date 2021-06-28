c8b543d97cb60be55ae1b95fd498b12571dba50b
sandereek <saner.eek@hcs-company.com>
2021-06-28 14:29:46 +0200

    HOMAAS Add Networklocations

---
+++ DE1-All-Networklocations.ps1
@@ -0,0 +1,6 @@
+. .\Add-NetworkLocation_on_WinServer.ps1
+
+Add-HEINEKENNetworkLocation -networkLocation "\\DE1SFILE01.DE1.HEIWAY.NET\HDEDepartments$"
+Add-HEINEKENNetworkLocation -networkLocation "\\DE1SFILE01.DE1.HEIWAY.NET\HDEManager$"
+Add-HEINEKENNetworkLocation -networkLocation "\\DE1SFILE01.DE1.HEIWAY.NET\HDEConfidential$"
+Add-HEINEKENNetworkLocation -networkLocation "\\DE1SFILE01.DE1.HEIWAY.NET\HDECollaboration$"
---
+++ RO1-Networklocations.ps1
@@ -0,0 +1,3 @@
+. .\Add-NetworkLocation_on_WinServer.ps1
+
+Add-HEINEKENNetworkLocation -networkLocation "\\10.145.146.205\apps$"
