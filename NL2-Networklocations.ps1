298b18f79da760dc1e6e9fe01c8366bff81d9285
sandereek <saner.eek@hcs-company.com>
2022-08-19 09:44:41 +0200

    Add Windows Network Locations

---
+++ NL2-Networklocations.ps1
@@ -0,0 +1,4 @@
+. .\Add-NetworkLocation_on_WinServer.ps1
+
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Business Services\Control Vrumona\Operationele Administratie - Management"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Business Services\Control Vrumona\Planning en Control Vrumona"
