3509ff9c0a60070d75d6f8b858f2473157134324
sandereek <saner.eek@hcs-company.com>
2023-08-21 10:00:25 +0200

    Add NetworkLocationsToWindowsServer

--- NL2-Networklocations.ps1
+++ NL2-Networklocations.ps1
@@ -3,7 +3,24 @@
 # Add-HEINEKENNetworkLocation -networkLocation "\\UNCpath\Share\Folder" -networklocationname "Networklocationname"
 # Network locations are stored in %appdata%\Microsoft\Windows\Network Shortcuts
 
-Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\dfsroot$\V\DataProj\wg-Manuals-Colonne1+2" 
-Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\dfsroot$\V\DataProj\wg-Manuals-Colonne3+9+10"
-Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\dfsroot$\V\DataProj\wg-Manuals-Colonne4"
-Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\dfsroot$\V\DataProj\wg-RayA-Tech"
+
+### Networklocations for 2023-08 Migrations
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\ASAfroepen"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Backup Scheduling"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Demand management"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\E-mail templates"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Fund applications"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Grondstoffen & Verpakkingen"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Handleidingen"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\home_obsolete"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Loonafvullers"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\MWR"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Persoonlijk"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Produktieplanning"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Projecten"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Sir Joe"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Stagiair"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\TPM"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\Unilever - Lipton"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\VMI_DeliveryProposal"
+Add-HEINEKENNetworkLocation -networkLocation "\\nl2sbunfile01.nl2.heiway.net\DepartmentData\Supply Chain\Supply Chain Planning\W-S&OP"
